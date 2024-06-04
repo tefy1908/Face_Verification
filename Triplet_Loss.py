d7b3e6f45845a8d82e556f22be69eafd9724ceb6
tefy1908 <tefymaherison19@gmail.com>
2024-06-04 18:30:59 +0200

    triplet suite avec bug ligne 477

--- Triplet_Loss.py
+++ Triplet_Loss.py
@@ -245,18 +245,277 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.models import load_model
 
 
-best_model_fname = "triplet_pretrained.h5"
-best_model_cb = ModelCheckpoint(best_model_fname, monitor='val_loss',
-                                save_best_only=True, verbose=1)
+#best_model_fname = "triplet_pretrained.h5"
+#best_model_cb = ModelCheckpoint(best_model_fname, monitor='val_loss',
+                                #save_best_only=True, verbose=1)
 
-history = model_triplet.fit(gen, 
-                    epochs=10,
-                    validation_data = gen_test,
-                    callbacks=[best_model_cb])
+#history = model_triplet.fit(gen, 
+                   # epochs=10,
+                    #validation_data = gen_test,
+                   # callbacks=[best_model_cb])
 
-plt.plot(history.history['loss'], label='train')
-plt.plot(history.history['val_loss'], label='validation')
+#plt.plot(history.history['val_loss'], label='validation')
+#plt.legend(loc='best')
+#model_triplet.load_weights("triplet_pretrained.h5")
+
+
+#problème : pas de GPU 
+
+
+#display similar image 
+emb = shared_conv.predict(all_imgs)
+emb = emb / np.linalg.norm(emb, axis=-1, keepdims=True)
+pixelwise = np.reshape(all_imgs, (all_imgs.shape[0], 60*60*3))
+
+def most_sim(idx, topn=5, mode="cosine"):
+    x = emb[idx]
+    if mode == "cosine":
+        x = x / np.linalg.norm(x)
+        sims = np.dot(emb, x)
+        ids = np.argsort(sims)[::-1]
+        return [(id,sims[id]) for id in ids[:topn]]
+    elif mode == "euclidean":
+        dists = np.linalg.norm(emb - x, axis=-1)
+        ids = np.argsort(dists)
+        return [(id,dists[id]) for id in ids[:topn]]
+    else:
+        dists = np.linalg.norm(pixelwise - pixelwise[idx], axis=-1)
+        ids = np.argsort(dists)
+        return [(id,dists[id]) for id in ids[:topn]]
+    
+def display(img):
+    img = img.astype('uint8')
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+
+interesting_classes = list(filter(lambda x: len(x[1])>4, classid_to_ids.items()))
+class_idx = random.choice(interesting_classes)[0]
+print(class_idx)
+img_idx = random.choice(classid_to_ids[class_idx])
+for id, sim in most_sim(img_idx):
+    display(all_imgs[id] + mean)
+    print((classid_to_name[id_to_classid[id]], id, sim))
+
+#test Recall @k model
+test_ids = []
+for class_id in range(split_num, num_classes-1):
+    img_ids = classid_to_ids[class_id]
+    if len(img_ids) > 1:
+        test_ids += img_ids
+
+print(len(test_ids))
+len([len(classid_to_ids[x]) for x in list(range(split_num, num_classes-1)) if len(classid_to_ids[x])>1])
+def recall_k(k=10, mode="embedding"):
+    num_found = 0
+    for img_idx in test_ids:
+        image_class = id_to_classid[img_idx]
+        found_classes = []
+        if mode == "embedding":
+            found_classes = [id_to_classid[x] for (x, score) in most_sim(img_idx, topn=k+1)[1:]]
+        elif mode == "random":
+            found_classes = [id_to_classid[x] for x in random.sample(
+                list(set(all_img_test_idx + all_img_train_idx) - {img_idx}), k)]
+        elif mode == "image":
+            found_classes = [id_to_classid[x] for (x, score) in most_sim(img_idx, topn=k+1, mode="image")[1:]]
+        if image_class in found_classes:
+            num_found += 1
+    return num_found / len(test_ids)
+
+recall_k(k=10), recall_k(k=10, mode="random")
+
+#hard negative mining 
+# Naive way to compute all similarities between all images. May be optimized!
+def build_similarities(conv, all_imgs):
+    embs = conv.predict(all_imgs)
+    embs = embs / np.linalg.norm(embs, axis=-1, keepdims=True)
+    all_sims = np.dot(embs, embs.T)
+    return all_sims
+
+def intersect(a, b):
+    return list(set(a) & set(b))
+
+def build_negatives(anc_idxs, pos_idxs, similarities, neg_imgs_idx, num_retries=20):
+    # If no similarities were computed, return a random negative
+    if similarities is None:
+        return random.sample(neg_imgs_idx,len(anc_idxs))
+    final_neg = []
+    # for each positive pair
+    for (anc_idx, pos_idx) in zip(anc_idxs, pos_idxs):
+        anchor_class = id_to_classid[anc_idx]
+        #positive similarity
+        sim = similarities[anc_idx, pos_idx]
+        # find all negatives which are semi(hard)
+        possible_ids = np.where((similarities[anc_idx] + 0.25) > sim)[0]
+        possible_ids = intersect(neg_imgs_idx, possible_ids)
+        appended = False
+        for iteration in range(num_retries):
+            if len(possible_ids) == 0:
+                break
+            idx_neg = random.choice(possible_ids)
+            if id_to_classid[idx_neg] != anchor_class:
+                final_neg.append(idx_neg)
+                appended = True
+                break
+        if not appended:
+            final_neg.append(random.choice(neg_imgs_idx))
+    return final_neg
+
+class HardTripletGenerator(tf.keras.utils.Sequence):
+    def __init__(self, Xa_train, Xp_train, batch_size, all_imgs, neg_imgs_idx, conv):
+        self.batch_size = batch_size
+        
+        self.imgs = all_imgs
+        self.Xa = Xa_train
+        self.Xp = Xp_train
+        self.num_samples = Xa_train.shape[0]
+        self.neg_imgs_idx = neg_imgs_idx
+
+        if conv:
+            print("Pre-computing similarities...", end=" ")
+            self.similarities = build_similarities(conv, self.imgs)
+            print("Done!")
+        else:
+            self.similarities = None
+        
+    def __len__(self):
+        return self.num_samples // self.batch_size
+        
+    def __getitem__(self, batch_index):
+        low_index = batch_index * self.batch_size
+        high_index = (batch_index + 1) * self.batch_size
+        
+        imgs_a = self.Xa[low_index:high_index]
+        imgs_p = self.Xp[low_index:high_index]
+        imgs_n = build_negatives(imgs_a, imgs_p, self.similarities, self.neg_imgs_idx)
+        
+        imgs_a = seq.augment_images(self.imgs[imgs_a])
+        imgs_p = seq.augment_images(self.imgs[imgs_p])
+        imgs_n = seq.augment_images(self.imgs[imgs_n])
+        
+        return ([imgs_a, imgs_p, imgs_n], np.zeros(shape=(imgs_a.shape[0])))
+    
+batch_size = 128
+gen_hard = HardTripletGenerator(Xa_train, Xp_train, batch_size, all_imgs, all_img_train_idx, shared_conv)
+print(len(gen_hard))
+[xa, xp, xn], y = gen_hard[0]
+print(xa.shape, xp.shape, xn.shape)
+
+plt.figure(figsize=(16, 9))
+
+for i in range(5):
+    plt.subplot(3, 5, i + 1)
+    plt.title("anchor")
+    plt.imshow((xa[i] + mean) / 255)
+    plt.axis('off')
+    
+for i in range(5):
+    plt.subplot(3, 5, i + 6)
+    plt.title("positive")
+    plt.imshow((xp[i] + mean) / 255)
+    plt.axis('off')
+    
+for i in range(5):
+    plt.subplot(3, 5, i + 11)
+    plt.title("negative")
+    plt.imshow((xn[i] + mean) / 255)
+    plt.axis('off')
+    
+plt.show()
+import keras
+
+class SharedConv2(tf.keras.Model):
+    """Improved version of SharedConv"""
+    def __init__(self):
+        super(SharedConv2, self).__init__(name="SharedConv2")
+
+        
+        self.conv1 = Conv2D(16, 3, activation="relu", padding="same")
+        self.conv2 = Conv2D(16, 3, activation="relu", padding="same")
+        self.pool1 = MaxPool2D((2,2)) # 30,30
+        self.conv3 = Conv2D(32, 3, activation="relu", padding="same")
+        self.conv4 = Conv2D(32, 3, activation="relu", padding="same")
+        self.pool2 = MaxPool2D((2,2)) # 15,15
+        self.conv5 = Conv2D(64, 3, activation="relu", padding="same")
+        self.conv6 = Conv2D(64, 3, activation="relu", padding="same")
+        self.pool3 = MaxPool2D((2,2)) # 8,8
+        self.conv7 = Conv2D(64, 3, activation="relu", padding="same")
+        self.conv8 = Conv2D(32, 3, activation="relu", padding="same")
+        self.flatten = Flatten()
+        self.dropout1 = Dropout(0.2)
+        self.fc1 = Dense(64)
+    
+    def call(self, inputs):
+        x = self.pool1(self.conv2(self.conv1(inputs)))
+        x = self.pool2(self.conv4(self.conv3(x)))
+        x = self.pool3(self.conv6(self.conv5(x)))
+        x = self.flatten(self.conv8(self.conv7(x)))
+        
+        return self.fc1(self.dropout1(x))
+    
+tf.random.set_seed(1337)
+shared_conv2 = SharedConv2()
+model_triplet2 = TripletNetwork(shared_conv2)
+
+opt = keras.optimizers.Adam(learning_rate=0.01)
+model_triplet2.compile(loss=identity_loss, optimizer=opt)
+
+gen_test = TripletGenerator(Xa_test, Xp_test, 32, all_imgs, all_img_test_idx)
+print(len(gen_test))
+# At first epoch we don't generate hard triplets so that our model can learn the easy examples first
+gen_hard = HardTripletGenerator(Xa_train, Xp_train, batch_size, all_imgs, all_img_train_idx, None)
+
+loss, val_loss = [], []
+
+best_model_fname_hard = "triplet_checkpoint_hard.h5"
+best_val_loss = float("inf")
+
+nb_epochs = 10
+for epoch in range(nb_epochs):
+    print("built new hard generator for epoch " + str(epoch))
+    
+    history = model_triplet2.fit(
+        gen_hard, 
+        epochs=1,
+        validation_data = gen_test)
+    loss.extend(history.history["loss"])
+    val_loss.extend(history.history["val_loss"])
+    
+    if val_loss[-1] < best_val_loss:
+        print("Saving best model")
+        model_triplet2.save_weights(best_model_fname_hard)
+    
+    gen_hard = HardTripletGenerator(Xa_train, Xp_train, batch_size, all_imgs, all_img_train_idx, shared_conv2)
+
+plt.plot(loss, label='train')
+plt.plot(val_loss, label='validation')
 plt.ylim(0, 0.5)
 plt.legend(loc='best')
 plt.title('Loss')
-model_triplet.load_weights("triplet_pretrained.h5")
+emb = shared_conv2.predict(all_imgs)
+emb = emb / np.linalg.norm(emb, axis=-1, keepdims=True)
+recall_k(k=10), recall_k(k=10, mode="random")
+
+shared_conv2_nohard = SharedConv2()
+model_triplet2_nohard = TripletNetwork(shared_conv2_nohard)
+
+opt = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+model_triplet2_nohard.compile(loss=identity_loss, optimizer=opt)
+
+gen_nohard = HardTripletGenerator(Xa_train, Xp_train, batch_size, all_imgs, all_img_train_idx, None)
+history = model_triplet2_nohard.fit_generator(
+        generator=gen_nohard, 
+        epochs=10,
+        validation_data=gen_test)
+
+plt.plot(loss, label='train (hardmining)')
+plt.plot(val_loss, label='validation (hardmining)')
+plt.plot(history.history["loss"], label="train")
+plt.plot(history.history["val_loss"], label="validation")
+plt.ylim(0, 0.5)
+plt.legend(loc='best')
+plt.title('Loss hardmining vs no hardmining')
+
+emb = shared_conv2_nohard.predict(all_imgs)
+emb = emb / np.linalg.norm(emb, axis=-1, keepdims=True)
+recall_k(k=10), recall_k(k=10, mode="random")
